21c241978f8b44a3e8139c5763962150858287c4
HenrickL <markhenrick.linsnagan@benilde.edu.ph>
2022-01-06 20:59:46 +0800

    mokc update

--- Frontend/mockIndex.php
+++ Frontend/mockIndex.php
@@ -2,6 +2,6 @@
   <head><title>Front -End</title></head>
   <body>
     <p><?php echo 'Hello Cleint,'; ?>
-    <br><?php echo 'sleep ends here'?></p>
+    <br><?php echo 'sleep ends here GGWP'?></p>
   </body>
 </html>
