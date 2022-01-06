3f6873e3159b8a690a4d0eb86eadf9340c64c002
HenrickL <markhenrick.linsnagan@benilde.edu.ph>
2022-01-06 20:41:03 +0800

    make folders and check

---
+++ Backend/mockIndex2.php
@@ -0,0 +1,7 @@
+<html>
+  <head><title>Front -End</title></head>
+  <body>
+    <p><?php echo 'Hello server,'; ?>
+    <br><?php echo 'sleep ends here'?></p>
+  </body>
+</html>
---
+++ Frontend/mockIndex.php
@@ -0,0 +1,7 @@
+<html>
+  <head><title>Front -End</title></head>
+  <body>
+    <p><?php echo 'Hello Cleint,'; ?>
+    <br><?php echo 'sleep ends here'?></p>
+  </body>
+</html>
