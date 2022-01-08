860cf5ea83d40eb1c747abc5719320825017c79c
HenrickL <markhenrick.linsnagan@benilde.edu.ph>
2022-01-08 14:33:08 +0800

    homework

--- View/mockIndex.php
+++ View/mockIndex.php
@@ -5,3 +5,13 @@
     <br><?php echo 'sleep ends here GGW'?></p>
   </body>
 </html>
+<!-- homework add your name in the page and upload the changes to git using the following commands
+
+git add . <- this records the changes you have done to the project
+git commit -m "changes" <- this commits the changes to be uploaded the content of the " " is a message about the changes
+git push <- uploads the commited changes
+
+git fetch <- this downloads the changes to your local PC
+
+
+-->
