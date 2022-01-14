99f82bc3051196344ee0ad966b147db04b7c6b3f
HenrickL <markhenrick.linsnagan@benilde.edu.ph>
2022-01-15 01:56:00 +0800

    begin logins

---
+++ View/loginView.php
@@ -0,0 +1,9 @@
+<div class ="Loginindex">
+    <h4>Login</h4>
+    <form action="Model/dbConnections/dbcon.php" method="post">
+        <input type="text" name="uid" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <br>
+        <button type="submit" name="submit">Login</button>
+    </form>
+</div>
