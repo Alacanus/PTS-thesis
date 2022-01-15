b2b4e129cb46061bcfa2fd58d82c54e51df14594
HenrickL <markhenrick.linsnagan@benilde.edu.ph>
2022-01-15 23:31:47 +0800

    hosting fix

--- Model/dbConnections/dbHandler.php
+++ Model/dbConnections/dbHandler.php
@@ -5,9 +5,9 @@
 class Hanlder {
     protected function connect() {
         try{
-            $username ="root";
-            $password ="";
-            $person1 = new PDO('mysql:host=localhost;dbname=pts', $username, $password);
+            $username ="u657624546_eslp";
+            $password ="Zxcvbnm1";
+            $person1 = new PDO('mysql:host=localhost;dbname=u657624546_pts', $username, $password);
             return $person1;
         }
         catch(PDOException $e){
