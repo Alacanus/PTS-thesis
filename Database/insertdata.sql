0abf4537c2febccd0d8d32a12b6451f47b3dedbc
HenrickL <markhenrick.linsnagan@benilde.edu.ph>
2022-02-06 02:46:20 +0800

    Fixed some sql insert data, also deleted redundant js file

--- Database/insertdata.sql
+++ Database/insertdata.sql
@@ -31,35 +31,42 @@ INSERT INTO PaymentStatus (description) VALUES ('Canceled');
 
 /* ----- Access Level Data ----- */
 
-INSERT INTO AccessLevel (accesType, description) VALUES ('Enrolled Learner', 'A Learner who has paid the course');
-INSERT INTO AccessLevel (accesType, description) VALUES ('Pending Enrollment', 'A Learner who has not paid the course');
-INSERT INTO AccessLevel (accesType, description) VALUES ('Cancel Enrollment', 'A Learner who does not want to continue the course');
+INSERT INTO AccessLevel (accessType, description) VALUES ('Enrolled Learner', 'A Learner who has paid the course');
+INSERT INTO AccessLevel (accessType, description) VALUES ('Pending Enrollment', 'A Learner who has not paid the course');
+INSERT INTO AccessLevel (accessType, description) VALUES ('Cancel Enrollment', 'A Learner who does not want to continue the course');
+
+/* ----- userrole Data ----- */
+INSERT INTO userroles (roleType) VALUES ('Admin');
+INSERT INTO userroles (roleType) VALUES ('Learner');
+INSERT INTO userroles (roleType) VALUES ('Instructor');
+INSERT INTO userroles (roleType) VALUES ('Coodinator');
+INSERT INTO userroles (roleType) VALUES ('Procurement');
 
 /* ----- Users Data ----- */
 
-INSERT INTO Users (username, email, password, firstname, lastname, blacklistID, auditID, active, activation_code) 
-VALUES ('genesis', 'genesis.fragas@benilde.edu.ph', 'Genesis', 'Fragas', 1, 1, 0, '0000');
-INSERT INTO Users (username, email, password, firstname, lastname, blacklistID, auditID, active, activation_code) 
-VALUES ('dennie', 'dennnie.go@benilde.edu.ph', 'Dennie', 'Go', 1, 1, 0, '0000');
-INSERT INTO Users (username, email, password, firstname, lastname, blacklistID, auditID, active, activation_code) 
-VALUES ('Samboi', 'samuelnarbuada@benilde.edu.ph', 'Samuel', 'Narbuada', 1, 1, 0, '0000');
-INSERT INTO Users (username, email, password, firstname, lastname, blacklistID, auditID, active, activation_code) 
-VALUES ('Markku', 'markhenrick.linsangan@benilde.edu.ph', 'Mark', 'Linsangan', 1, 1, 0, '0000');
-INSERT INTO Users (username, email, password, firstname, lastname, blacklistID, auditID, active, activation_code) 
-VALUES ('shadowpick', 'shadow.pick@benilde.edu.ph', 'Shadow', 'Pick', 1, 1, 0, '0000');
+INSERT INTO Users (username, email, password, firstname, lastname, roleID, active, activation_code) 
+VALUES ('genesis', 'genesis.fragas@benilde.edu.ph', 'Zxcvbnmz12#', 'Genesis', 'Fragas', 1, 1, '0000');
+INSERT INTO Users (username, email, password, firstname, lastname, roleID, active, activation_code) 
+VALUES ('dennie', 'dennnie.go@benilde.edu.ph', 'Zxcvbnmz12#', 'Dennie', 'Go', 1, 1, '0000');
+INSERT INTO Users (username, email, password, firstname, lastname, roleID, active, activation_code) 
+VALUES ('Samboi', 'samuelnarbuada@benilde.edu.ph', 'Zxcvbnmz12#', 'Samuel', 'Narbuada', 1, 1, '0000');
+INSERT INTO Users (username, email, password, firstname, lastname, roleID, active, activation_code) 
+VALUES ('Markku', 'markhenrick.linsangan@benilde.edu.ph', 'Zxcvbnmz12#', 'Mark', 'Linsangan', 1, 1, '0000');
+INSERT INTO Users (username, email, password, firstname, lastname , roleID, active, activation_code) 
+VALUES ('shadowpick', 'shadow.pick@benilde.edu.ph', 'Zxcvbnmz12#', 'Shadow', 'Pick', 1, 1, '0000');
 
 /* ----- UserProfile Data ----- */
 
 INSERT INTO UserProfile (age, gender, birthday, address, contactno, aboutme, userID) 
-VALUES ('22', 'M', '01/04/2000', '#00 St. Galor Westmanor Maybuhay Pasig City', 'A person you can vibe with', 1);
+VALUES ('22', 'M', '01/04/2000', '#00 St. Galor Westmanor Maybuhay Pasig City', '639', 'A person you can vibe with', 2);
 INSERT INTO UserProfile (age, gender, birthday, address, contactno, aboutme, userID) 
-VALUES ('23', 'M', '06/12/1999', '#00 St. Galor Westmanor Maybuhay Pasig City', 'A person you can vibe with', 2);
+VALUES ('23', 'M', '06/12/1999', '#00 St. Galor Westmanor Maybuhay Pasig City', '639', 'A person you can vibe with', 3);
 INSERT INTO UserProfile (age, gender, birthday, address, contactno, aboutme, userID) 
-VALUES ('24', 'M', '12/05/1998', '#00 St. Galor Westmanor Maybuhay Pasig City', 'A person you can vibe with', 3);
+VALUES ('24', 'M', '12/05/1998', '#00 St. Galor Westmanor Maybuhay Pasig City', '639', 'A person you can vibe with', 4);
 INSERT INTO UserProfile (age, gender, birthday, address, contactno, aboutme, userID) 
-VALUES ('21', 'M', '05/02/2001', '#00 St. Galor Westmanor Maybuhay Pasig City', 'A person you can vibe with', 4);
+VALUES ('21', 'M', '05/02/2001', '#00 St. Galor Westmanor Maybuhay Pasig City', '639', 'A person you can vibe with', 5);
 INSERT INTO UserProfile (age, gender, birthday, address, contactno, aboutme, userID) 
-VALUES ('20', 'F', '02/16/2002', '#00 St. Galor Westmanor Maybuhay Pasig City', 'A person you can vibe with', 5);
+VALUES ('20', 'F', '02/16/2002', '#00 St. Galor Westmanor Maybuhay Pasig City', '639', 'A person you can vibe with', 6);
 
 /* ----- BlackList Data ----- */
 
@@ -71,11 +78,11 @@ INSERT INTO Blacklist (status, description) VALUES ('Whitelisted', 'User is free
 
 /* ----- AuditTrail Data ----- */
 
-INSERT INTO AuditTrail (Log, tableName, userID, actionID) VALUES ('', '', 0, 0);
-INSERT INTO AuditTrail (Log, tableName, userID, actionID) VALUES ('', '', 0, 0);
-INSERT INTO AuditTrail (Log, tableName, userID, actionID) VALUES ('', '', 0, 0);
-INSERT INTO AuditTrail (Log, tableName, userID, actionID) VALUES ('', '', 0, 0);
-INSERT INTO AuditTrail (Log, tableName, userID, actionID) VALUES ('', '', 0, 0);
+INSERT INTO AuditTrail (logs, tableName, userID, actionID) VALUES ('', '', 2, 2);
+INSERT INTO AuditTrail (logs, tableName, userID, actionID) VALUES ('', '', 3, 2);
+INSERT INTO AuditTrail (logs, tableName, userID, actionID) VALUES ('', '', 6, 2);
+INSERT INTO AuditTrail (logs, tableName, userID, actionID) VALUES ('', '', 5, 2);
+INSERT INTO AuditTrail (logs, tableName, userID, actionID) VALUES ('', '', 4, 2);
 
 /* ----- Packages Data ----- */
 
@@ -84,4 +91,6 @@ INSERT INTO Package (description) VALUES ('Porkchop');
 INSERT INTO Package (description) VALUES ('Beef');
 INSERT INTO Package (description) VALUES ('Wagyu');
 INSERT INTO Package (description) VALUES ('Eggs');
+
+
 
