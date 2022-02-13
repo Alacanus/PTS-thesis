e209c92d3e5398594290d4258d1e7e0e215794e1
unknown <genesis.fragas@benilde.edu.ph>
2022-02-13 14:50:15 +0800

    N/A

--- public/login.php
+++ public/login.php
@@ -8,6 +8,7 @@
 require __DIR__ . '/../src/bootstrap.php';
 require __DIR__ . '/../src/login.php';
 ?>
+<link rel="stylesheet" href="<?= 'http://'. $_SERVER['HTTP_HOST'] . '/PTS-thesis/public/css/style.css'?>">
 <script src="https://www.google.com/recaptcha/api.js"></script>
 <script>
    function onSubmit(token) {
@@ -20,32 +21,39 @@ require __DIR__ . '/../src/login.php';
         <?= $errors['login'] ?>
     </div>
 <?php endif ?>
-<main id="mymain1">
-    <form id="form" action="login.php" method="post" >
-        <h1>Login</h1>
-        <div>
-            <label for="username">Username:</label>
-            <input type="text" name="username" id="username" class="<?= error_class($errors, 'username') ?>">
-        <small><?= $errors['username'] ?? '' ?></small>
+<div class="login">
+    <div class="center-form">
+        <div class="conainter-login">
+            <main id="mymain1">
+                <form id="form" action="login.php" method="post" >
+                        <h2>Login</h2>
+                        <div class="form-container">
+                            <div class="form-element">
+                                <label for="username">Username:</label>
+                                <input type="text" name="username" id="username" class="<?= error_class($errors, 'username') ?>">
+                            <small><?= $errors['username'] ?? '' ?></small>
+                            </div>
+                            <div class="form-element">
+                                <label for="password">Password:</label>
+                                <input type="password" name="password" id="password"  class="<?= error_class($errors, 'password') ?>">
+                            <small><?= $errors['password'] ?? '' ?></small>
+                            </div>
+                            <div class="form-group">
+                                <div class="g-recaptcha" data-sitekey="6Le6_lQeAAAAAG_6B4F-OjL0mbth_UQLUihCtxiG">
+                                </div>
+                                <small id="captcha_error" class="text-danger"></small>
+                                <button>Login</button>
+                            </div>
+                            <div class="form-element">
+                                <a href="register.php">Register</a>
+                            </div>
+                            <div class="form-element">
+                                <a href="changepassword/requestChange.php">Change Password</a>
+                            </div>
+                        </div>
+                </form>
+            </main>
         </div>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password"  class="<?= error_class($errors, 'password') ?>">
-        <small><?= $errors['password'] ?? '' ?></small>
-        </div>
-        <div class="form-group">
-            <div class="g-recaptcha" data-sitekey="6Le6_lQeAAAAAG_6B4F-OjL0mbth_UQLUihCtxiG">
-            </div>
-            <small id="captcha_error" class="text-danger"></small>
-            <button
-            >Login</button>
-        </div>
-        <section>
-            <a href="register.php">Register</a>
-        </section>
-        <section>
-            <a href="changepassword/requestChange.php">Change Password</a>
-        </section>
-    </form>
-</main>
+    </div>
+</div>
 <?php view('footer') ?>
