4556ffe9ecdff8c9cd87fab8d6023926e13c218f
unknown <genesis.fragas@benilde.edu.ph>
2022-03-10 22:08:07 +0800

    Added Navigation for Admin, Procurement, Coordinator and Instructor Roles

---
+++ public/navigationtemp.php
@@ -0,0 +1,215 @@
+<?php
+require __DIR__ . '/../src/bootstrap.php';
+?>
+
+<?php view('header', ['title' => 'Change Password']); ?>
+
+<!--======================== LoggedIn - Learner ========================-->
+<!-- <nav class="nav-loggedin">
+    <div class="row">
+        <div class="logo">
+            <h2>
+                <div class="logo-blue">P</div>TS
+            </h2>
+        </div>
+        <ul class="main-nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="aboutUs.php">About Us</a></li>
+            <li><a href="#">Courses</a></li>
+            <li><a href="viewfaq.php">FAQ</a></li>
+            <li>
+                <div class="user-img" onclick="umToggle_01();">
+                    <img src="/PTS-thesis/static/profilepic.jpg" alt="profile picture.jpg">
+                </div>
+                <div class="user-menu">
+                    <div class="user-content">
+                        <p>Genesis Fragas</p>
+                        <p id="content--1">Learner</p>
+                    </div>
+                    <ul>
+                        <li><img src="/PTS-thesis/static/user-solid.svg" alt=""><a href="#">My Profile</a></li>
+                        <li><img src="/PTS-thesis/static/chalkboard-solid.svg" alt=""><a href="#">My Classes</a></li>
+                        <li><i class="bi bi-calendar-event"></i><a href="#">Schedules</a></li>
+                        <li><img src="/PTS-thesis/static/truck-fast-solid.svg" alt=""><a href="#">Deliveries</a></li>
+                        <li><i class="bi bi-cart"></i><a href="#">Order</a></li>
+                        <li><img src="/PTS-thesis/static/table-solid.svg" alt=""><a href="#">Transaction</a></li>
+                        <li><img src="/PTS-thesis/static/audittrail-solid.svg" alt=""><a href="#">Audit Trail</a></li>
+                        <li><img src="/PTS-thesis/static/logout.svg" alt=""><a href="#">Logout</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav> -->
+
+<!--======================== LoggedIn - Instructor ========================-->
+<!-- <nav class="nav-loggedin">
+    <div class="row">
+        <div class="logo">
+            <h2>
+                <div class="logo-blue">P</div>TS
+            </h2>
+        </div>
+        <ul class="main-nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="aboutUs.php">About Us</a></li>
+            <li><a href="#">Courses</a></li>
+            <li><a href="viewfaq.php">FAQ</a></li>
+            <li>
+                <div class="user-img" onclick="umToggle_02();">
+                    <img src="/PTS-thesis/static/profilepic.jpg" alt="profile picture.jpg">
+                </div>
+                <div class="user-menu">
+                    <div class="user-content">
+                        <p>Mark Linsangan</p>
+                        <p id="content--1">Instructor</p>
+                    </div>
+                    <ul>
+                        <li><img src="/PTS-thesis/static/user-solid.svg" alt=""><a href="#">My Profile</a></li>
+                        <li><img src="/PTS-thesis/static/chalkboard-solid.svg" alt=""><a href="#">My Classes</a></li>
+                        <li><i class="bi bi-people"></i><a href="#">Learner List</a></li>
+                        <li><i class="bi bi-calendar-event"></i><a href="#">Schedules</a></li>
+                        <li><img src="/PTS-thesis/static/table-solid.svg" alt=""><a href="#">Transaction</a></li>
+                        <li><img src="/PTS-thesis/static/audittrail-solid.svg" alt=""><a href="#">Audit Trail</a></li>
+                        <li><img src="/PTS-thesis/static/logout.svg" alt=""><a href="#">Logout</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav> -->
+
+<!--======================== LoggedIn - Coordinator ========================-->
+<!-- <nav class="nav-loggedin">
+    <div class="row">
+        <div class="logo">
+            <h2>
+                <div class="logo-blue">P</div>TS
+            </h2>
+        </div>
+        <ul class="main-nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="aboutUs.php">About Us</a></li>
+            <li><a href="#">Courses</a></li>
+            <li><a href="viewfaq.php">FAQ</a></li>
+            <li>
+                <div class="user-img" onclick="umToggle_03();">
+                    <img src="/PTS-thesis/static/profilepic.jpg" alt="profile picture.jpg">
+                </div>
+                <div class="user-menu">
+                    <div class="user-content">
+                        <p>Dennie Go</p>
+                        <p id="content--1">Coordinator</p>
+                    </div>
+                    <ul>
+                        <li><img src="/PTS-thesis/static/user-solid.svg" alt=""><a href="#">My Profile</a></li>
+                        <li><i class="bi bi-file-earmark-person-fill"></i><a href="#">Class Management</a></li>
+                        <li><i class="bi bi-calendar-event"></i><a href="#">Schedules</a></li>
+                        <li><img src="/PTS-thesis/static/table-solid.svg" alt=""><a href="#">Transaction</a></li>
+                        <li><img src="/PTS-thesis/static/audittrail-solid.svg" alt=""><a href="#">Audit Trail</a></li>
+                        <li><img src="/PTS-thesis/static/logout.svg" alt=""><a href="#">Logout</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav> -->
+
+<!--======================== LoggedIn - Procurement ========================-->
+<!-- <nav class="nav-loggedin">
+    <div class="row">
+        <div class="logo">
+            <h2>
+                <div class="logo-blue">P</div>TS
+            </h2>
+        </div>
+        <ul class="main-nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="aboutUs.php">About Us</a></li>
+            <li><a href="#">Courses</a></li>
+            <li><a href="viewfaq.php">FAQ</a></li>
+            <li>
+                <div class="user-img" onclick="umToggle_04();">
+                    <img src="/PTS-thesis/static/profilepic.jpg" alt="profile picture.jpg">
+                </div>
+                <div class="user-menu">
+                    <div class="user-content">
+                        <p>Samuel Narbuada</p>
+                        <p id="content--1">Procurement</p>
+                    </div>
+                    <ul>
+                        <li><i class="bi bi-cart"></i><a href="#">Order</a></li>
+                        <li><img src="/PTS-thesis/static/truck-fast-solid.svg" alt=""><a href="#">Delivery</a></li>
+                        <li><img src="/PTS-thesis/static/table-solid.svg" alt=""><a href="#">Transaction</a></li>
+                        <li><img src="/PTS-thesis/static/audittrail-solid.svg" alt=""><a href="#">Audit Trail</a></li>
+                        <li><img src="/PTS-thesis/static/logout.svg" alt=""><a href="#">Logout</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav> -->
+
+<!--======================== LoggedIn - Admin ========================-->
+<nav class="nav-loggedin">
+    <div class="row">
+        <div class="logo">
+            <h2>
+                <div class="logo-blue">P</div>TS
+            </h2>
+        </div>
+        <ul class="main-nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="aboutUs.php">About Us</a></li>
+            <li><a href="#">Courses</a></li>
+            <li><a href="viewfaq.php">FAQ</a></li>
+            <li>
+                <div class="user-img" onclick="umToggle_04();">
+                    <img src="/PTS-thesis/static/profilepic.jpg" alt="profile picture.jpg">
+                </div>
+                <div class="user-menu">
+                    <div class="user-content">
+                        <p>Genesis Fragas</p>
+                        <p id="content--1">Admin</p>
+                    </div>
+                    <ul>
+                        <li><img src="/PTS-thesis/static/user-solid.svg" alt=""><a href="#">Account Management</a></li>
+                        <li><i class="bi bi-file-earmark-person-fill"></i><a href="#">Class Management</a></li>
+                        <li><img src="/PTS-thesis/static/table-solid.svg" alt=""><a href="#">Transaction</a></li>
+                        <li><img src="/PTS-thesis/static/audittrail-solid.svg" alt=""><a href="#">Audit Trail</a></li>
+                        <li><img src="/PTS-thesis/static/logout.svg" alt=""><a href="#">Logout</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<!--======================== Script for Img Menu Drop Down ========================-->
+
+<script>
+    function umToggle_01() {
+        const toggleUM = document.querySelector('.user-menu--01');
+        toggleUM.classList.toggle('active')
+    }
+
+    function umToggle_02() {
+        const toggleUM = document.querySelector('.user-menu');
+        toggleUM.classList.toggle('active')
+    }
+
+    function umToggle_03() {
+        const toggleUM = document.querySelector('.user-menu');
+        toggleUM.classList.toggle('active')
+    }
+
+    function umToggle_04() {
+        const toggleUM = document.querySelector('.user-menu');
+        toggleUM.classList.toggle('active')
+    }
+
+    function umToggle_05() {
+        const toggleUM = document.querySelector('.user-menu');
+        toggleUM.classList.toggle('active')
+    }
+</script>
--- src/inc/header.php
+++ src/inc/header.php
@@ -8,11 +8,11 @@
     <meta name="description" content="ELP is a Electronic Learning PLatform for students who want to continuesly hone their skills">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <!-- JavaScript Bundle with Popper //remove with own js css/js-->
+    
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
-
     <!-- Design - External CSS & Scripts Code -->
     <link rel="stylesheet" type="text/css" href="<?= 'http://' . $_SERVER['HTTP_HOST'] . '/PTS-thesis/public/css/grid.css' ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -36,7 +36,7 @@
 
 <body>
     <header>
-        <nav class="not-loggedin">
+        <!-- <nav class="not-loggedin">
             <div class="row">
                 <div class="logo">
                     <h2>
@@ -50,39 +50,6 @@
                     <li><a href="viewfaq.php">FAQ</a></li>
                     <li><a href="login.php"><button class="btn btn-nav btn-full">LOGIN</button></a></li>
                     <li><a href="register.php"><button class="btn btn-nav btn-nav-ghost btn-ghost">REGISTER</button></a></li>
-                </ul>
-            </div>
-        </nav>
-        <!-- <nav class="nav-loggedin">
-            <div class="row">
-                <div class="logo">
-                    <h2>
-                        <div class="logo-blue">P</div>TS
-                    </h2>
-                </div>
-                <ul class="main-nav">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="aboutUs.php">About Us</a></li>
-                    <li><a href="#">Courses</a></li>
-                    <li><a href="viewfaq.php">FAQ</a></li>
-                    <li>
-                        <div class="user-img" onclick="umToggle();">
-                            <img src="/PTS-thesis/static/profilepic.jpg" alt="profile picture.jpg">
-                        </div>
-                        <div class="user-menu">
-                            <div class="user-content">
-                                <p>Genesis Fragas</p>
-                                <p id="content--1">Learner</p>
-                            </div>
-                            <ul>
-                                <li><img src="/PTS-thesis/static/user-solid.svg" alt=""><a href="#">My Profile</a></li>
-                                <li><img src="/PTS-thesis/static/chalkboard-solid.svg" alt=""><a href="#">My Classes</a></li>
-                                <li><img src="/PTS-thesis/static/table-solid.svg" alt=""><a href="#">Transaction</a></li>
-                                <li><img src="/PTS-thesis/static/audittrail-solid.svg" alt=""><a href="#">Audit Trail</a></li>
-                                <li><img src="/PTS-thesis/static/logout.svg" alt=""><a href="#">Logout</a></li>
-                            </ul>
-                        </div>
-                    </li>
                 </ul>
             </div>
         </nav> -->
